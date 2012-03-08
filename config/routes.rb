9009b59b4e1bf62caf8d6d77e4d8c023c4baa3ca
Vojtech Salbaba <vojtech.salbaba@friendlysystems.cz>
2012-03-08 21:41:26 +0100

    routy pro rails 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
-ActionController::Routing::Routes.draw do |map|
+Rails.application.routes.draw do |map|
   match 'paperclipdb/*dir_name/:file_name.:format' => 'paperclipdb/attachments#get_attachment'
 end
